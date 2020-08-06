b67cce704b76a587f1366aeccf80debfb650e274
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-08-06 04:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function map(sourceArray, fn) {
   return sourceArray.map(fn)
 }
 
-function reduce(sourceArray, fn, startingPoint) {
+function reduce(sourceArray, fn, startingPoint=0) {
   return sourceArray.reduce(fn)
 }
