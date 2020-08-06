fb08b809df27ac03b279fc7d1b9bef4d4089a05c
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-08-06 04:09:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ function map(sourceArray, fn) {
 }
 
 function reduce(sourceArray, fn, startingPoint) {
+  if (typeof startingPoint == 'undefined') {
     return sourceArray.reduce(fn)
+  } else {
+    return sourceArray.reduce(fn) + startingPoint
+  }
 }
